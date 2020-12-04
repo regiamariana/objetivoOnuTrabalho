0f7ac156728be157af07ff713cbc4d1e632e98e0
regiamariana <reginhamari03@gmail.com>
2020-12-04 01:32:03 -0300

    up

--- AGOSTO VERDE/JS/script.js
+++ AGOSTO VERDE/JS/script.js
@@ -1,24 +1,68 @@
-const debounce = function name(func, wait, immediate) {
-    let timeout;
-    return function(...args) {
-        const context = this;
-        const later = function(){
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        const callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if(callNow) func.apply(context, args);
-    };
-};
+// const debounce = function name(func, wait, immediate) {
+//     let timeout;
+//     return function(...args) {
+//         const context = this;
+//         const later = function(){
+//             timeout = null;
+//             if (!immediate) func.apply(context, args);
+//         };
+//         const callNow = immediate && !timeout;
+//         clearTimeout(timeout);
+//         timeout = setTimeout(later, wait);
+//         if(callNow) func.apply(context, args);
+//     };
+// };
+
+
+const node = document.querySelector('.hello')
+
+
+node.addEventListener('mousemove', function(){
+    this.classList.add('hello-active');
+});
+node.addEventListener('mouseout', function(){
+    this.classList.remove('hello-active');
+});
+
+
+const node2 = document.querySelector('.hello2')
+node2.addEventListener('mousemove', function(){
+    this.classList.add('hello-active2');
+});
+node2.addEventListener('mouseout', function(){
+    this.classList.remove('hello-active2');
+});
+
+
+const node3 = document.querySelector('.hello3')
+node3.addEventListener('mousemove', function(){
+    this.classList.add('hello-active3');
+});
+node3.addEventListener('mouseout', function(){
+    this.classList.remove('hello-active3');
+});
+
+
+const node4 = document.querySelector('.hello4')
+node4.addEventListener('mousemove', function(){
+    this.classList.add('hello-active4');
+});
+node4.addEventListener('mouseout', function(){
+    this.classList.remove('hello-active4');
+});
+
+
+// node.addEventListener('mouseout', function(){
+//     this.classList.remove('edon');
+// });
+
 
 const target = document.querySelectorAll('[data-anime]')
 const animationClass = 'animate';
 
 
 function animeScroll() {
-    const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+    const windowTop = window.pageYOffset;
 
     target.forEach(function(element) {
         if ((windowTop) > element.offsetTop) {
@@ -29,13 +73,18 @@ function animeScroll() {
         // console.log(element.offsetTop)
     })
 }
-
-animeScroll();
-
-
-if (target.length) {
-window.addEventListener('scroll', debounce(function(){
+window.addEventListener('scroll', (function(){
     animeScroll();
     console.log('nhnhnhn')
-}, 200))
-}
+}))
+
+
+
+
+
+
+// animeScroll();
+
+
+
+
